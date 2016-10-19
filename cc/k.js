25c376335ad64d74999d9c000ae4035583f123d4
unknown <>
2016-10-19 19:04:25 +0800

    update express instance

--- cc/k.js
+++ cc/k.js
@@ -1,20 +1,47 @@
 var express = require('express');
 //var bp = require('body-parser');
 var app = express();
+var crypro = require('crypto');
 
 var log4js = require('log4js');
 log4js.configure('conf.json', { cwd: ''});
-//app.set('trust proxy', true);
+app.set('trust proxy', true);
 var logger = log4js.getLogger('CC');
 
 app.set('view engine', 'ejs');
 var d = __dirname + '/v';
 app.set('views', d);
 
+function sha1(str){
+	var md5 = crypto.createHash('sha1');
+	md5.update(str);
+	str = md5.digest('hex');
+	return str;
+}
+
+var conf = {
+	appID: 'wx3dade2f3d2a28f00',
+	appSecret: '',
+	token: 'chichimonde'
+}
+
+
 app.get('/', function(req, res){
-    var data = {numbers : [1, 2, 3, 4, 5]};
-    res.render('index',data);
-    logger.info('['+ req.ip + '] [' + req.method + '] [' + req.originalUrl + ']');
+	var token = conf.token;
+	var signature = req.query.signature;
+	var echostr = req.query.echostr;
+	var timestamp = req.query.timestamp;
+	var nonce = req.query.nonce;
+	var st = [token, timestamp, nonce].sort().join('');
+	var cryptSt = sha1(st);
+	if (cryptSt === signature) {
+		res.end(echostr);
+	}
+	else {
+		res.end('ERROR');
+	}
+    //res.render('index',data);
+    logger.info('['+ req.ips + '] [' + req.method + '] [' + req.originalUrl + ']');
 });
 
 app.listen(1234);
